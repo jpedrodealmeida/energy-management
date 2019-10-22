796f0dcd1c82c7b91085ffa69db01ece4a75a8a1
joaos <joaos@DESKTOP-5NRPFBG>
2019-10-22 20:56:07 -0300

    panel structure and design

--- energy-management/src/App.js
+++ energy-management/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
 import './App.css';
 import MainPainel from './components/main-painel/MainPainel';
 import UserProfile from './components/user-profile/UserProfile';
@@ -8,12 +11,17 @@ import Graphics from './components/graphics/Graphics';
 function App() {
   return (
     <div className="App">
-      <div className="painel-structure">
-        <MainPainel />
-        <UserProfile />
-      </div>
-      <Graphics />
+      <Card>
+        <CardContent>
+          <div className="painel-structure">
+            <MainPainel />
+            <UserProfile />
+          </div>
+          <Graphics />
+        </CardContent>
+      </Card>
     </div>
+   
   );
 }
 
--- energy-management/src/components/main-painel/MainPainel.js
+++ energy-management/src/components/main-painel/MainPainel.js
@@ -1,20 +1,11 @@
 import React, {Component} from 'react'
 
-import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 
 import './MainPainel.css'
-
-const useStyles = makeStyles(theme => ({
-    button: {
-      width: '500px'
-    },
-    input: {
-      display: 'none',
-    },
-  }));
 
 
 class MainPainel extends Component{
@@ -30,9 +21,12 @@ class MainPainel extends Component{
             backgroundColor: '#00FFFA',
             color: 'white'
             },
-            userName:{
+            userName: {
                 color: "#00000066",
                 textAlign: "initial"
+            },
+            textField: {
+                
             }
         }
     }
@@ -48,6 +42,73 @@ class MainPainel extends Component{
                         <div className="button-structure">
                             <Button style={classes.button}>
                                 SAVE CHANGES
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="body-card-structure">
+                        
+                        <div className="up-value">
+                            <span className="label-data">
+                                Data 1
+                            </span>
+                            <div className="input-structure">
+                                <div className="input-up-value" >
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                </div>
+                                <div className="input-up-value" >
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="up-value">
+                            <span className="label-data">
+                                Data 2
+                            </span>
+                            <div className="input-structure">
+                                <div className="input-up-value" >
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                </div>
+                                <div className="input-up-value" >
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" style={classes.textField} margin="normal" variant="outlined" />
+                                </div>
+                            </div>
+                        </div>
+                        <hr className="line-result" />
+                        <div className="up-value">
+                            <span className="label-data">
+                                +
+                            </span>
+                            <div className="input-structure">
+                                <div className="input-up-value" >
+                                    <TextField id="outlined-bare" disabled="true" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" disabled="true" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" disabled="true" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" disabled="true" style={classes.textField} margin="normal" variant="outlined" />
+                                </div>
+                                <div className="input-up-value" >
+                                    <TextField id="outlined-bare" disabled="true" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" disabled="true" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" disabled="true" style={classes.textField} margin="normal" variant="outlined" />
+                                    <TextField id="outlined-bare" disabled="true" style={classes.textField} margin="normal" variant="outlined" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="button-graphic">
+                            <Button style={classes.button}>
+                                GENERATE GRAPH
                             </Button>
                         </div>
                     </div>
