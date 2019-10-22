0cc70d7d2f7a642065618bd0b1c04da3ab39dcaf
joaos <joaos@DESKTOP-5NRPFBG>
2019-10-22 18:32:13 -0300

    tittle panel card

--- energy-management/src/components/main-painel/MainPainel.js
+++ energy-management/src/components/main-painel/MainPainel.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react'
 
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 
 import './MainPainel.css'
+
+const useStyles = makeStyles(theme => ({
+    button: {
+      width: '500px'
+    },
+    input: {
+      display: 'none',
+    },
+  }));
+
 
 class MainPainel extends Component{
     constructor(props){
@@ -12,14 +24,32 @@ class MainPainel extends Component{
         this.state = {
             name: ''
         }
+        
+        this.styleClass = {
+            button: {
+            backgroundColor: '#00FFFA',
+            color: 'white'
+            },
+            userName:{
+                color: "#00000066",
+                textAlign: "initial"
+            }
+        }
     }
 
+    
     render(){
+        const classes = this.styleClass;
         return (
             <Card>
                 <CardContent>
-                    <div>
-                        <span>Main Painel Works!!!</span>
+                    <div className="title-card-structure">
+                        <span style={classes.userName}>Hello Johnn</span>
+                        <div className="button-structure">
+                            <Button style={classes.button}>
+                                SAVE CHANGES
+                            </Button>
+                        </div>
                     </div>
                 </CardContent>
             </Card>
